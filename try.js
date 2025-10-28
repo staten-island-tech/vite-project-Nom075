95cf0566fe1546f1d09dff0b214804e04d1ecb46
Nom075 <graytonl@nycstudents.net>
2025-10-28 11:02:17 -0700

    A

---
+++ try.js
@@ -0,0 +1,22 @@
+/* function angryProfessor(k, a){
+    let Present = 0
+    a.forEach((student)=>{
+        if (student <= 0){
+            Present += 1
+        }
+    })
+    if (Present >= k){
+        return "NO"
+    }
+    else{
+        return "YES"
+    }
+
+
+
+
+
+}
+
+
+console.log(angryProfessor(2, [0, 1, 2, 3])) */
